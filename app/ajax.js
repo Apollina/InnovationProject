161dc0d36fb6c2578373612e479d9c4350099231
apollina <apollinariia.gainulenko@gmail.com>
2018-11-21 03:22:20 +0200

    a bit clean up

--- app/ajax.js
+++ app/ajax.js
@@ -1,15 +1,11 @@
 const apiHost = 'https://linkedcourses-api.test.hel.ninja/linkedcourses-test/v1'
-//const apiHost = 'https://linkedcourses-api.test.hel.ninja/linkedcourses-test/v1/events/?format=api'
 
 export default {
     async fetchInitialCourses() {
         try {
             const response = await fetch(apiHost + '/event/?format=json');
             const responseJson = await response.json();
-            console.log('RESPONSE JSON' + responseJson);
-            console.log(responseJson);
             console.log(Object.keys(responseJson).length);
-            console.log("test json read");
             console.log('ID' + responseJson.data);
 
             let key, count = 0;
@@ -19,7 +15,6 @@ export default {
                 }
             }
             console.log('COUNT' + count);
-
 
             return responseJson;
         } catch (error) {
@@ -31,21 +26,6 @@ export default {
         try {
             const response = await fetch(apiHost + '/event/?format=json' + courseId);
             const responseJson = await response.json();
-            console.log('RESPONSE JSON' + responseJson);
-            console.log(responseJson);
-            console.log(Object.keys(responseJson).length);
-            console.log("test json read");
-            console.log('ID' + responseJson.data);
-
-            let key, count = 0;
-            for(key in responseJson) {
-                if(responseJson.hasOwnProperty(key)) {
-                    count++;
-                }
-            }
-            console.log('COUNT' + count);
-
-
             return responseJson;
         } catch (error) {
             console.error(error);
