63e9cdbc1ed84b19486dc456ac6ed5a0937724ee
apollina <apollinariia.gainulenko@gmail.com>
2018-11-21 13:31:49 +0200

    quick bottom tab nav change

--- app/index.js
+++ app/index.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import { TabNav } from "./config/BottomNavigation";
-//import CoursesList from "./views/OfferedCourses/CoursesList";
-import Keywords from "./views/Chatbot/Keywords";
 
 export default class App extends Component {
     render() {
