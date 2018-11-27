1eb65e2d512207b7011cd3bb22b65870e7047763
Helga <hs082@hdm-stuttgart.de>
2018-11-27 19:40:25 +0200

    Get user data from DB

--- app/views/Profile/ProfileInfo.js
+++ app/views/Profile/ProfileInfo.js
@@ -1,52 +1,56 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Image, View } from 'react-native'
 import { Text } from 'native-base'
 import * as Progress from 'react-native-progress'
 import styles from '../../styles';
 import RenderCourses from './RenderCourses';
+import PropTypes from "prop-types";
 
-const ProfileInfo = props => {
-    const {stateData: {
-        nickName, signInDate, userLevel, levelProgression, coursesVisited, activeCourses
-        }
-    } = props;
-    return (
-        <View >
-            <Text style={styles.H1}>
-                {nickName}
-            </Text>
-            <View style={styles.infoWrapper}>
-                <Image source={{uri: 'http://placekitten.com/320/180'}} style={styles.profilePicture} />
-                <View style={styles.textWrapper}>
-                    <Text style={styles.H2}>Member since</Text>
-                    <Text style={styles.smallText}>{signInDate}</Text>
+class ProfileInfo extends Component {
+    static propTypes = {
+        userInfo: PropTypes.object.isRequired
+    };
+
+    render() {
+        return (
+            <View>
+                <Text style={styles.H1}>
+                    {this.props.userInfo.nickName}
+                </Text>
+                <View style={styles.infoWrapper}>
+                    <Image source={{uri: 'http://placekitten.com/320/180'}} style={styles.profilePicture}/>
+                    <View style={styles.textWrapper}>
+                        <Text style={styles.H2}>Member since</Text>
+                        <Text style={styles.smallText}>{this.props.userInfo.signInDate}</Text>
+                    </View>
                 </View>
-            </View>
-            <Text style={styles.H1}>
-                Level {userLevel}
-            </Text>
-            <Text>
-                Level progress
-            </Text>
-            <Progress.Bar style={styles.progressBar} progress={levelProgression} height={10} width={null} borderWidth={2} borderColor={'black'} borderRadius={10}/>
-            {coursesVisited && coursesVisited.length &&       
+                <Text style={styles.H1}>
+                    Level {this.props.userInfo.userLevel}
+                </Text>
+                <Text>
+                    Level progress
+                </Text>
+                <Progress.Bar style={styles.progressBar} progress={this.props.userInfo.levelProgression} height={10} width={null}
+                              borderWidth={2} borderColor={'black'} borderRadius={10}/>
+                {this.props.userInfo.coursesVisited && this.props.userInfo.coursesVisited.length &&
                 <View>
                     <Text style={styles.courseHeader}>
                         Last course locations visited
                     </Text>
-                    <RenderCourses coursesToMap={coursesVisited} />
+                    <RenderCourses coursesToMap={this.props.userInfo.coursesVisited}/>
                 </View>
-            }
-            {activeCourses && activeCourses.length &&
+                }
+                {this.props.userInfo.activeCourses && this.props.userInfo.activeCourses.length &&
                 <View>
                     <Text style={styles.courseHeader}>
                         Active courses
                     </Text>
-                    <RenderCourses coursesToMap={activeCourses} />
+                    <RenderCourses coursesToMap={this.props.userInfo.activeCourses}/>
                 </View>
-            }
-        </View>
-    );
+                }
+            </View>
+        );
+    }
 }
 
 export default ProfileInfo;
--- app/views/Profile/ProfilePage.js
+++ app/views/Profile/ProfilePage.js
@@ -1,64 +1,54 @@
 import React, {Component} from 'react'
-import { ScrollView, View, TextInput } from 'react-native'
-import { Text, Container, Button } from 'native-base'
+import {ScrollView, View, TextInput} from 'react-native'
+import {Text, Container, Button} from 'native-base'
 import DatePicker from 'react-native-datepicker'
 import styles from '../../styles';
+import firebase from '../../firebase';
 import ProfileInfo from './ProfileInfo';
 import RenderCourses from './RenderCourses';
 
 class ProfilePage extends Component {
     constructor(props) {
         super(props);
-        this.state = { 
+        this.state = {
             showEditing: false,
-            nickName: "Ben Lonely",
-            signInDate: '1.10.2018',
-            userLevel: '4',
-            levelProgression: 0.7,
-            coursesVisited: [
-                {
-                    courseName: 'Taekwondo',
-                    courseLocation: 'Asiakkaankatu, Helsinki',
-                    courseDate: '25.10.2018'
-                },
-                {
-                    courseName: 'Pitsinnypläys',
-                    courseLocation: 'Asiakkaankatu, Helsinki',
-                    courseDate: '25.10.2018'
-                }
-            ],
-            activeCourses: [
-                {
-                    courseName: 'Taekwondo',
-                    courseLocation: 'Asiakkaankatu, Helsinki',
-                },
-                {
-                    courseName: 'Pitsinnypläys',
-                    courseLocation: 'Asiakkaankatu, Helsinki',
-                }
-            ],
+            userInfo: {},
             date: new Date()
         };
     }
-    
+
+    async componentDidMount() {
+        this.getUserInfo();
+    }
+
+    getUserInfo() {
+        firebase.database().ref('userList/').once('value', (snapshot) => {
+            console.log(snapshot.val()[0]);
+            this.setState({userInfo: snapshot.val()[0]});
+        }).catch((error) => {
+            //error callback
+            console.log('[ProfilePage] DB getUserInfo() error ', error)
+        })
+    }
+
     render() {
-        return ( 
+        return (
             <Container style={styles.pageContainer}>
                 <ScrollView style={styles.viewWrapper}>
-                    {!this.state.showEditing &&  
-                       <ProfileInfo stateData={this.state} /> 
+                    {!this.state.showEditing &&
+                    <ProfileInfo userInfo={this.state.userInfo}/>
                     }
-                    {this.state.showEditing &&  
+                    {this.state.showEditing &&
                     <View>
                         <View style={styles.inputWrapper}>
                             <Text style={[styles.flex1, styles.inputHeader]}>Nickname: </Text>
-                            <TextInput 
+                            <TextInput
                                 style={{
                                     flex: 1,
                                     fontSize: 20
                                 }}
                                 onChangeText={(nickName) => this.setState({nickName})}
-                                value={this.state.nickName}
+                                value={this.state.userInfo.nickName}
                                 maxLength={20}
                             />
                         </View>
@@ -86,28 +76,31 @@ class ProfilePage extends Component {
                                         display: 'none'
                                     }
                                 }}
-                                onDateChange={(date) => {this.setState({date: date})}}
+                                onDateChange={(date) => {
+                                    this.setState({date: date})
+                                }}
                             />
                         </View>
                         <Text style={styles.smallText}>
                             Only used for course recommendations
                         </Text>
-                        {this.state.activeCourses && this.state.activeCourses.length &&
-                            <View style={{
-                                marginTop: 50
-                            }}>
-                                <Text style={styles.courseHeader}>
-                                    Active courses
-                                </Text>
-                                <RenderCourses coursesToMap={this.state.activeCourses} />
-                            </View>
+                        {this.state.userInfo.activeCourses && this.state.userInfo.activeCourses.length &&
+                        <View style={{
+                            marginTop: 50
+                        }}>
+                            <Text style={styles.courseHeader}>
+                                Active courses
+                            </Text>
+                            <RenderCourses coursesToMap={this.state.userInfo.activeCourses}/>
+                        </View>
                         }
                     </View>
                     }
-                    <Button bordered rounded dark style={styles.editButton} onPress={()=> this.setState({showEditing: !this.state.showEditing})}>
+                    <Button bordered rounded dark style={styles.editButton}
+                            onPress={() => this.setState({showEditing: !this.state.showEditing})}>
                         {this.state.showEditing ? <Text> Save changes </Text> : <Text> Edit </Text>}
-                    </Button>   
-                </ScrollView>                
+                    </Button>
+                </ScrollView>
             </Container>
         );
     }
